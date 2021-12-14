79f6cd8b627c8227fa7d0e79858089dd3c5e4c02
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2021-12-15 00:16:05 +0100

    change background

--- TD4/index.js
+++ TD4/index.js
@@ -1,5 +1,17 @@
-document.getElementById("btn").addEventListener("click", myFunction);
+// var v1 = document.getElementsByClassName("melek");
+// v1.addEventListener("click", function () {
+//   alert("aslma");
+// });
 
-function myFunction() {
-  document.getElementById("demo").innerHTML = "Hello World";
-}
+// function affiche() {
+//   return document.write(
+//     `<h1>le titre de ce document ${window.document.title}</h1>`,
+//   );
+// }
+
+document.getElementsByTagName("button").addEventListener(
+  "click",
+  setTimeout(() => {
+    document.write(`aslema`);
+  }, 3000),
+);
